880ddb51439affcd323b6d3d367986ac0cce5c6c
juanFGQ <juanfelipegq@outlook.es>
2024-01-30 14:33:46 +0100

    checkbox chiptheme theme

---
+++ lib/utils/theme/custom_themes/checkbox_theme.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class TCheckBoxTheme {
+  TCheckBoxTheme._();
+
+  static CheckboxThemeData lightCheckBoxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+    checkColor: MaterialStateColor.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: MaterialStateColor.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.blue;
+      } else {
+        return Colors.transparent;
+      }
+    }),
+  );
+  static CheckboxThemeData darkCheckBoxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+    checkColor: MaterialStateColor.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: MaterialStateColor.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.blue;
+      } else {
+        return Colors.transparent;
+      }
+    }),
+  );
+}
