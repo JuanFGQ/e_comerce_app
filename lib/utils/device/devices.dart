b1aebf4f7ce18cba25cd4a8f3c7cbb03ab2c0e82
juanFGQ <juanfelipegq@outlook.es>
2024-01-31 08:34:44 +0100

    formatters

--- lib/utils/device/devices.dart
+++ lib/utils/device/devices.dart
@@ -1,44 +1,113 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'dart:io';
 
-class TDeviceUtils {
-  static void hideKeyboard(BuildContext context) {
-    FocusScope.of(context).requestFocus(FocusNode());
-  }
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
 
-  static Future<void> setStatusBarColor(Color color) async {
-    SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(statusBarColor: color));
-  }
 
-  static bool isLandscapeOrientation(BuildContext context) {
-    final viewInsets = View.of(context).viewInsets;
-    return viewInsets.bottom == 0;
-  }
+//FIX THE ERROR RELATED WITH GET.context!
 
-  static bool isPortraitOrientation(BuildContext context) {
-    final viewInsets = View.of(context).viewInsets;
-    return viewInsets.bottom != 0;
-  }
+// class TDeviceUtils {
+//   static void hideKeyboard(BuildContext context) {
+//     FocusScope.of(context).requestFocus(FocusNode());
+//   }
 
-  static void setFullScreen(bool enable) {
-    SystemChrome.setEnabledSystemUIMode(
-        enable ? SystemUiMode.immersiveSticky : SystemUiMode.edgeToEdge);
-  }
+//   static Future<void> setStatusBarColor(Color color) async {
+//     SystemChrome.setSystemUIOverlayStyle(
+//         SystemUiOverlayStyle(statusBarColor: color));
+//   }
 
-  static double getScreenHeight() {
-    return MediaQuery.of(Get.context!).size.height;
-  }
+//   static bool isLandscapeOrientation(BuildContext context) {
+//     final viewInsets = View.of(context).viewInsets;
+//     return viewInsets.bottom == 0;
+//   }
 
-  static double getScreenWidth() {
-    return MediaQuery.of(Get.context!).size.width;
-  }
+//   static bool isPortraitOrientation(BuildContext context) {
+//     final viewInsets = View.of(context).viewInsets;
+//     return viewInsets.bottom != 0;
+//   }
 
-  static double getPixelRatio() {
-    return MediaQuery.of(Get.context!).devicePixelRatio;
-  }
+//   static void setFullScreen(bool enable) {
+//     SystemChrome.setEnabledSystemUIMode(
+//         enable ? SystemUiMode.immersiveSticky : SystemUiMode.edgeToEdge);
+//   }
 
-  static double getStatusBarHeight(){
-    return MediaQuery.of(context!)
-  }
-}
+//   // static double getScreenHeight() {
+//   //   return MediaQuery.of(Get.context!).size.height;
+//   // }
+
+//   // static double getScreenWidth() {
+//   //   return MediaQuery.of(Get.context!).size.width;
+//   // }
+
+//   static double getPixelRatio() {
+//     return MediaQuery.of(Get.context!).devicePixelRatio;
+//   }
+
+//   static double getStatusBarHeight() {
+//     return MediaQuery.of(Get.context!).padding.top;
+//   }
+
+//   static double getBottomNavigationBarHeight() {
+//     return kBottomNavigationBarHeight;
+//   }
+
+//   static double getAppBarHeight() {
+//     return kToolbarHeight;
+//   }
+
+//   static double getKeyboardHeight() {
+//     final viewInsets = View.of(Get.context!).viewInsets;
+//     return viewInsets.bottom > 0;
+//   }
+
+//   static Future<bool> isPhysicalDevice() async {
+//     return defaultTargetPlatform == TargetPlatform.android ||
+//         defaultTargetPlatform == TargetPlatform.iOS;
+//   }
+
+//   static void vibrate(Duration duration) {
+//     HapticFeedback.vibrate();
+//     Future.delayed(duration, () => HapticFeedback.vibrate());
+//   }
+
+//   static Future<void> setPreferredOrientations(
+//       List<DeviceOrientation> orientarions) async {
+//     await SystemChrome.setPreferredOrientations(orientarions);
+//   }
+
+//   static void hideStatusBar() {
+//     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+//   }
+
+//   static void showStatusBar() {
+//     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+//         overlays: SystemUiOverlay.values);
+//   }
+
+//   static Future<bool> hasInternetConnection() async {
+//     try {
+//       final result = await InternetAddress.lookup('example.com');
+//       return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+//     } on SocketException catch (_) {
+//       return false;
+//     }
+//   }
+
+//   static bool isIOS() {
+//     return Platform.isIOS;
+//   }
+
+//   static bool isAndroid() {
+//     return Platform.isAndroid;
+//   }
+
+//   static void launchUrl(String url) async {
+//     if (await canLaunchUrlString(url)) {
+//       await launchUrlString(url);
+//     } else {
+//       throw 'Could not launch $url';
+//     }
+//   }
+// }
+
+
---
+++ lib/utils/formatters/formatter.dart
@@ -0,0 +1,23 @@
+
+
+//ITS NEEDED TO IMPORT INTL PACKAGE FOR DELETE THIS ERROR 
+
+
+class TFormatter {
+  static String formatDate(DateTime? date) {
+    date ??= DateTime.now();
+    return DateFormat('dd-MM-yyyy').format(date);
+  }
+
+
+  static String formatCurrency(double amount){
+    return NumberFormat.currency(locale:'en_US',symbol; '\$').format(amount);
+
+  }
+
+  static String formatPhoneNumber(String phoneNumber){
+    if(phoneNumber.length == 10){
+      return '(${phoneNumber.substring(0,3)}) ${phoneNumber.substring(3,6)} ${phoneNumber.substring(6)}'
+    }
+  }
+}
