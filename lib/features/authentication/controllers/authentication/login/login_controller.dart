27dc9ba5901f35502779e05da941dd8830acc973
JuanFGQ <juanfelipegq@outlook.es>
2024-02-10 22:37:41 +0100

    USER LOGIN DONE

--- lib/features/authentication/controllers/authentication/login/login_controller.dart
+++ lib/features/authentication/controllers/authentication/login/login_controller.dart
@@ -1,3 +1,65 @@
+import 'package:e_comerce_app/common/widgets/loaders/loaders.dart';
+import 'package:e_comerce_app/data/repositories/authentication_repository.dart';
+import 'package:e_comerce_app/features/authentication/controllers/network_manager.dart';
+import 'package:e_comerce_app/utils/constants/image_strings.dart';
+import 'package:e_comerce_app/utils/popups/full_screen_loader.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-class LoginController extends GetxController {}
+class LoginController extends GetxController {
+  //VARIABLES
+  final rememberMe = false.obs;
+  final hidePassword = true.obs;
+  final localStorage = GetStorage();
+  final email = TextEditingController();
+  final password = TextEditingController();
+  GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+  @override
+  void onInit() {
+    email.text = localStorage.read('REMEMBER_ME_EMAIL');
+    password.text = localStorage.read('REMEMBER_ME_PASSWORD');
+
+    super.onInit();
+  }
+
+  Future<void> emailAndPasswordSignIn() async {
+    try {
+//START LOADING
+      TFullScreenLoader.openLoadingDialog(
+          'Loggin you in...', TImages.handLoading);
+
+      //CHECK INTERNET CONNECTIVITY
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        TFullScreenLoader.stopLoading();
+        return;
+      }
+
+      //FORM VALIDATION
+      if (!loginFormKey.currentState!.validate()) {
+        TFullScreenLoader.stopLoading();
+        return;
+      }
+
+      //SAVE DATA IF REMEMEBER ME IS SELECTED
+      if (rememberMe.value) {
+        localStorage.write('REMEMBER_ME_EMAIL', email.text.trim());
+        localStorage.write('REMEMBER_ME_PASSWORD', password.text.trim());
+      }
+
+      //LOGIN USER USING EMAIL & PASSWORD AUTHENTICATION
+      final userCredentials = await AuthenticationRepository.instance
+          .loginWithEmailAndPassword(email.text.trim(), password.text.trim());
+
+      //REMOVE LOADER
+      TFullScreenLoader.stopLoading();
+
+      //REDIRECT
+      AuthenticationRepository.instance.screenRedirect();
+    } catch (e) {
+      TFullScreenLoader.stopLoading();
+      TLoaders.errorSnackBar(title: 'Oh Snap!', message: e.toString());
+    }
+  }
+}
--- lib/features/authentication/screens/authentication/login/widgets/login_form.dart
+++ lib/features/authentication/screens/authentication/login/widgets/login_form.dart
@@ -1,8 +1,10 @@
+import 'package:e_comerce_app/features/authentication/controllers/authentication/login/login_controller.dart';
 import 'package:e_comerce_app/features/authentication/screens/authentication/forget_password/forget_password.dart';
 import 'package:e_comerce_app/features/authentication/screens/authentication/signup/signup.dart';
 import 'package:e_comerce_app/navigation_menu.dart';
 import 'package:e_comerce_app/utils/constants/sizes.dart';
 import 'package:e_comerce_app/utils/constants/text_strings.dart';
+import 'package:e_comerce_app/utils/validators/validators.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
@@ -14,22 +16,37 @@ class TLoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(LoginController());
     return Form(
+      key: controller.loginFormKey,
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: TSizes.spaceBtwSection),
         child: Column(
           children: [
             //EMAIL
             TextFormField(
+                validator: (value) => TValidator.validateEmail(value),
+                controller: controller.email,
                 decoration: const InputDecoration(
                     prefix: Icon(Iconsax.direct_right),
                     labelText: TTexts.email)),
             const SizedBox(height: TSizes.spaceBtwInputFields),
             //PASSWORD
-            TextFormField(
-                decoration: const InputDecoration(
-                    prefix: Icon(Iconsax.password_check),
-                    labelText: TTexts.password)),
+            Obx(
+              () => TextFormField(
+                  obscureText: controller.hidePassword.value,
+                  validator: (value) => TValidator.validatePassword(value),
+                  controller: controller.password,
+                  decoration: InputDecoration(
+                      prefix: Icon(Iconsax.password_check),
+                      labelText: TTexts.password,
+                      suffixIcon: IconButton(
+                          onPressed: () => controller.hidePassword.value =
+                              !controller.hidePassword.value,
+                          icon: Icon(controller.hidePassword.value
+                              ? Iconsax.eye_slash
+                              : Iconsax.eye)))),
+            ),
             const SizedBox(height: TSizes.spaceBtwInputFields / 2),
 
             //REMEMBER ME & FORGET PASSWORD
@@ -38,7 +55,10 @@ class TLoginForm extends StatelessWidget {
               children: [
                 Row(
                   children: [
-                    Checkbox(value: true, onChanged: (value) {}),
+                    Obx(() => Checkbox(
+                        value: controller.rememberMe.value,
+                        onChanged: (value) => controller.rememberMe.value =
+                            !controller.rememberMe.value)),
                     const Text(TTexts.rememberMe)
                   ],
                 ),
@@ -52,7 +72,7 @@ class TLoginForm extends StatelessWidget {
             SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
-                    onPressed: () => Get.to(() => const NavigationMenu()),
+                    onPressed: () => controller.emailAndPasswordSignIn(),
                     child: const Text(TTexts.signIn))),
             SizedBox(
                 width: double.infinity,
