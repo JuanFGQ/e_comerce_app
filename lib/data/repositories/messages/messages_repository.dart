13f236be615457d2a35adde46accd77d1bb8384a
juanFGQ <juanfelipegq@outlook.es>
2024-02-26 14:29:49 +0100

    building send and receive message methods

--- lib/data/repositories/messages/messages_repository.dart
+++ lib/data/repositories/messages/messages_repository.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_comerce_app/features/messages/models/messages_mode.dart';
 import 'package:e_comerce_app/features/shop/models/poduct_model.dart';
 import 'package:e_comerce_app/utils/exceptions/firebase_exceptions.dart';
 import 'package:e_comerce_app/utils/exceptions/format_exception.dart';
@@ -12,12 +13,39 @@ class MessagingRepository extends GetxController {
 
   final FirebaseFirestore _db = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
-  User get authUser => _auth.currentUser!;
 
-  //SEND MESSAGES
-  Future<void> sendMessages(ProductModel model) async {
+  //!SEND MESSAGES
+  Future<void> sendMessages(
+      MessagingModel newMessage, String chatRoomID) async {
     try {
-      await _db.collection("Products").doc().set(model.toJson());
+      await _db
+          .collection("ChatRooms")
+          .doc(chatRoomID)
+          .collection('messages')
+          .add(newMessage.toJson());
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. please try again';
+    }
+  }
+
+  //!FETCH MESSAGES
+
+  Stream<QuerySnapshot> fetchMessages(String chatRoomID) {
+    try {
+      _db
+          .collection("ChatRooms")
+          .doc(chatRoomID)
+          .collection('messages')
+          .orderBy("timeStamp", descending: false)
+          .snapshots();
+
+          return _db.
     } on FirebaseException catch (e) {
       throw TFirebaseException(e.code).message;
     } on FormatException catch (_) {
--- lib/features/messages/controllers/messages_controller.dart
+++ lib/features/messages/controllers/messages_controller.dart
@@ -1,6 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_comerce_app/common/widgets/loaders/loaders.dart';
+import 'package:e_comerce_app/data/repositories/messages/messages_repository.dart';
+import 'package:e_comerce_app/features/authentication/controllers/network/network_manager.dart';
+import 'package:e_comerce_app/features/messages/models/messages_mode.dart';
+import 'package:e_comerce_app/utils/constants/image_strings.dart';
+import 'package:e_comerce_app/utils/popups/full_screen_loader.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:get/state_manager.dart';
 
 class MessagesController extends GetxController {
   static MessagesController get instance => Get.find();
+  final _auth = FirebaseAuth.instance;
+  final messageRepo = Get.put(MessagingRepository());
+  final _db = FirebaseFirestore.instance;
+  Future sendMessages(String receiverID, message) async {
+    try {
+      TFullScreenLoader.openLoadingDialog(
+          'Storing Addres...', TImages.handLoading);
+
+      //check internet connectivity
+
+      final isConneted = await NetworkManager.instance.isConnected();
+      if (!isConneted) {
+        TFullScreenLoader.stopLoading();
+        return;
+      }
+      final Timestamp timestamp = Timestamp.now();
+      final currentUserID = _auth.currentUser!.uid;
+      final currentUserEmail = _auth.currentUser!.email!;
+//send message model
+      final messageM = MessagingModel(
+          senderID: currentUserID,
+          senderEmail: currentUserEmail,
+          receiverID: receiverID,
+          message: message,
+          timestamp: timestamp);
+
+      //construct chat rrom ID for the two users(sorted to ensure uniqueness)
+      List<String> ids = [currentUserID, receiverID];
+      ids.sort(); //sort the ids (this ensure the chattoomID is the same for any 2 people)
+      String chatRoomID = ids.join('_');
+
+      //SEND/STORE new message to firebase
+      await messageRepo.sendMessages(messageM, chatRoomID);
+    } catch (e) {
+      TLoaders.errorSnackBar(
+          title: 'Something went wrong try again later', message: e.toString());
+    }
+  }
+
+  Future getMessages() {}
 }
