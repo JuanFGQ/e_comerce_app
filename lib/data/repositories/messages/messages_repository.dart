12d41a7bbfdf1e8df31264859ed83e64a022802b
juanFGQ <juanfelipegq@outlook.es>
2024-02-26 08:50:41 +0100

    building messaging feature

--- lib/data/repositories/messages/messages_repository.dart
+++ lib/data/repositories/messages/messages_repository.dart
@@ -1,1 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_comerce_app/features/shop/models/poduct_model.dart';
+import 'package:e_comerce_app/utils/exceptions/firebase_exceptions.dart';
+import 'package:e_comerce_app/utils/exceptions/format_exception.dart';
+import 'package:e_comerce_app/utils/exceptions/platform_exception.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 
+class MessagingRepository extends GetxController {
+  static MessagingRepository get instance => Get.find();
+
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
+  User get authUser => _auth.currentUser!;
+
+  //SEND MESSAGES
+  Future<void> sendMessages(ProductModel model) async {
+    try {
+      await _db.collection("Products").doc().set(model.toJson());
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. please try again';
+    }
+  }
+}
---
+++ lib/features/messages/controllers/messages_controller.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+import 'package:get/state_manager.dart';
+
+class MessagesController extends GetxController {
+  static MessagesController get instance => Get.find();
+}
